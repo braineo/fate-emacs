8dd75f04f3e64704d24fbd6f218d7d9950b5bc07
Binbin Ye <phantom2501@gmail.com>
2018-12-15 13:37:19 +0900

    Init commit

---
+++ core/core-load-paths.el
@@ -0,0 +1,25 @@
+(defun add-to-load-path (dir) (add-to-list 'load-path dir))
+
+(defvar fate-directory
+  user-emacs-directory
+  "Root directory of Fate Emacs")
+
+(defconst fate-core-directory
+  (expand-file-name "core" fate-directory)
+  "Fate core directory")
+
+(defconst fate-modules-directory
+  (expand-file-name "modules" fate-directory)
+  "Fate modules directory")
+
+(defconst user-home-directory
+  (expand-file-name "~/")
+  "User home directory (~/).")
+
+;; load paths
+(mapc 'add-to-load-path
+      `(
+        ,fate-core-directory
+        ,fate-modules-directory))
+        
+(provide 'core-load-paths)
---
+++ init.el
@@ -0,0 +1,67 @@
+(setq load-prefer-newer t)
+(load (concat (file-name-directory load-file-name)
+              "core/core-load-paths.el")
+      nil (not init-file-debug))
+;; (require 'core-load-paths)
+(setq custom-file (expand-file-name "custom.el" fate-directory))
+
+;; Package configs
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+                         ("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+
+;; Minimal UI
+(scroll-bar-mode -1)
+(tool-bar-mode   -1)
+(tooltip-mode    -1)
+(menu-bar-mode   -1)
+
+;; Font
+(add-to-list 'default-frame-alist '(font . "Operator Mono"))
+(add-to-list 'default-frame-alist '(height . 24))
+(add-to-list 'default-frame-alist '(width . 80))
+
+;; Theme
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-one t))
+
+;; Which Key
+(use-package which-key
+  :ensure t
+  :init
+  (setq which-key-separator " ")
+  (setq which-key-prefix-prefix "+")
+  :config
+  (which-key-mode 1))
+
+(use-package parinfer
+  :ensure t
+  :bind
+  (("C-," . parinfer-toggle-mode))
+  :init
+  (progn
+    (setq parinfer-extensions
+          '(defaults       ; should be included.
+            pretty-parens  ; different paren styles for different modes.
+            evil           ; If you use Evil.
+            lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
+            paredit        ; Introduce some paredit commands.
+            smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+            smart-yank))   ; Yank behavior depend on mode.
+    (add-hook 'clojure-mode-hook #'parinfer-mode)
+    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'common-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'scheme-mode-hook #'parinfer-mode)
+    (add-hook 'lisp-mode-hook #'parinfer-mode)))
