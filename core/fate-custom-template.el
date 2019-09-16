9896b856fcdd6a0ce46464d34431be33b2f7a377
Binbin Ye <binbin.ye@mujin.co.jp>
2019-09-16 13:59:01 +0900

    Fix noto fonts check

--- core/fate-custom-template.el
+++ core/fate-custom-template.el
@@ -11,11 +11,11 @@
   (when (and (x-list-fonts "Operator Mono"))
     (let ((font "Operator Mono:weight=light:style=book:pixelsize=12"))
       ;; (set-frame-font font)
-      (add-to-list 'default-frame-alist `(font . ,font)))))
+      (add-to-list 'default-frame-alist `(font . ,font))))
 
-(when (x-list-fonts "Noto Sans")
-  (set-fontset-font t 'han "Noto Sans CJK SC Regular")
-  (set-fontset-font t 'kana "Noto Sans CJK JP Regular")
-  (set-fontset-font t 'cjk-misc "Noto Sans CJK Sc Regular"))
+  (when (x-list-fonts "Noto Sans")
+    (set-fontset-font t 'han "Noto Sans CJK SC Regular")
+    (set-fontset-font t 'kana "Noto Sans CJK JP Regular")
+    (set-fontset-font t 'cjk-misc "Noto Sans CJK Sc Regular")))
 
 ;;; fate-custom.el ends here
