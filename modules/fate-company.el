30f8c623e55f1d920250098220eb77720524a38f
Binbin Ye <binbin.ye@mujin.co.jp>
2020-05-15 12:01:57 +0900

    Add more company configs

--- modules/fate-company.el
+++ modules/fate-company.el
@@ -32,17 +32,22 @@
    :map company-active-map
    ("C-p" . company-select-previous)
    ("C-n" . company-select-next)
-   ("TAB" . company-complete-common-or-cycle)
    ("<tab>" . company-complete-common-or-cycle)
    :map company-search-map
    ("C-p" . company-select-previous)
    ("C-n" . company-select-next))
-  :config
-  (setq company-tooltip-align-annotations t ; aligns annotation to the right
-        company-tooltip-limit 12            ; bigger popup window
-        company-idle-delay .5               ; decrease delay before autocompletion popup shows
-        company-minimum-prefix-length 2
-        company-require-match nil))
+  :custom
+  (company-tooltip-align-annotations t "aligns annotation to the right")
+  (company-tooltip-limit 12 "bigger popup window")
+  (company-idle-delay .5 "decrease delay before autocompletion popup shows")
+  (company-minimum-prefix-length 1)
+  (company-require-match nil)
+  (company-dabbrev-ignore-case nil)
+  (company-dabbrev-downcase nil)
+  (company-global-modes '(not erc-mode message-mode help-mode gud-mode eshell-mode shell-mode)))
+
+(use-package company-prescient
+  :hook (company . company-prescient-mode))
 
 (provide 'fate-company)
 ;;; fate-company.el ends here
