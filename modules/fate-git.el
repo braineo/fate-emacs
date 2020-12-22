14c3d8c5f60c91cb1c9d7852f2894f9ff058d6de
Binbin Ye <binbin.ye@mujin.co.jp>
2020-12-22 09:43:09 +0900

    Let tranient popup show up at buffer

--- modules/fate-git.el
+++ modules/fate-git.el
@@ -19,6 +19,7 @@
   :custom
   (magit-revision-show-gravatars '("^Author:     " . "^Commit:     "))
   (magit-display-buffer-function 'magit-display-buffer-fullcolumn-most-v1)
+  (magit-diff-refine-hunk t "Show fine differences for the current diff hunk only.")
   :bind
   (("C-x g" . magit-status)
    ("s-m" . magit-file-dispatch)))
@@ -44,6 +45,7 @@
     ;; There's no fringe when Emacs is running in the console
     (diff-hl-margin-mode 1)))
 
+;; Has some serious performance issue for diff > 2000 line changes, disable for now.
 (use-package magit-delta
   :disabled
   :if (executable-find "delta")
@@ -59,6 +61,6 @@
   (transient-history-file (concat fate-cache-directory "transient/history.el"))
   (transient-values-file (concat fate-cache-directory "transient/values.el"))
   (transient-levels-file (concat fate-cache-directory "transient/levels.el"))
-  (magit-diff-refine-hunk t "Show fine differences for the current diff hunk only."))
+  (transient-display-buffer-action '(display-buffer-below-selected)))
 (provide 'fate-git)
 ;;; fate-git.el ends here
