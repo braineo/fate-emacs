3220ccdca47d640c987e6041ff08c7b18ef8fa6c
Binbin Ye <binbin.ye@mujin.co.jp>
2022-03-28 20:21:58 +0900

    replace jsons-print-path with tree-sitter-version

--- modules/fate-web.el
+++ modules/fate-web.el
@@ -24,6 +24,7 @@
 
 ;;; Code:
 
+(require 'tree-sitter)
 
 (use-package prettier-js
   :defer t
@@ -40,6 +41,43 @@
         (prettier-js-mode)
         (cl-return)))))
 
+(defun fate/json-get-path (current-node output)
+  "Get path to json value at cursor position.  CURRENT-NODE is a tree-sitter-node.
+OUTPUT is parsed path list."
+  (let* ((parent-node (tsc-get-parent current-node)))
+    (if parent-node
+      (progn
+        (when (eq (tsc-node-type parent-node) 'array)
+          (let ((index -1)
+                (cursor (tsc-make-cursor parent-node)))
+            (tsc-goto-first-child cursor)
+            (while (not (tsc-node-eq current-node (tsc-current-node cursor)))
+                (progn
+                  (tsc-goto-next-sibling cursor)
+                  (if (tsc-node-named-p (tsc-current-node cursor))
+                    (progn
+                      (setq index (+ index 1))))))
+            (setq output (push index output))))
+        (when (eq (tsc-node-type current-node) 'pair)
+            (setq output (push (tsc-node-text (tsc-get-nth-child current-node 0)) output)))
+        (fate/json-get-path parent-node output))
+      output)))
+
+(defun fate/json-print-path-js ()
+  "Copy json path in JavaScript format."
+  (interactive)
+  (let (json-path)
+    (dolist (elt (fate/json-get-path (tree-sitter-node-at-pos) '()) json-path)
+      (when (stringp elt)
+        (let* ((trimmed-elt (string-trim elt "\"" "\"")))
+          (if (string-match-p "-" trimmed-elt)
+            (setq json-path (concat json-path "[" trimmed-elt "]"))
+            (setq json-path (concat json-path "." trimmed-elt)))))
+      (when (numberp elt)
+        (setq json-path (concat json-path "[" (number-to-string elt) "]"))))
+    (message json-path)
+    (kill-new json-path)))
+
 (use-package json-mode
   :mode "\\.json?\\'"
   :config
@@ -52,7 +90,8 @@
   (json-mode . fate/prettier-minor-mode)
   :bind
   (:map json-mode-map
-    ("C-c C-l" . fate/json-prettier)))
+    ("C-c C-l" . fate/json-prettier)
+    ("C-c C-p" . fate/json-print-path-js)))
 
 (use-package less-css-mode
   :hook (less-css-mode . fate/prettier-minor-mode))
