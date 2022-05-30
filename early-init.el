9682d1e1fc770de7978b85916c7d827898d5c13f
Binbin Ye <phantom2501@gmail.com>
2022-05-31 01:11:47 +0900

    hide eln-cache folder

--- early-init.el
+++ early-init.el
@@ -40,6 +40,11 @@
 
 (setq package-enable-at-startup nil) ;; don't auto-initialize!
 
+(when (fboundp 'startup-redirect-eln-cache)
+  (startup-redirect-eln-cache
+   (convert-standard-filename
+      (expand-file-name  "var/eln-cache/" user-emacs-directory))))
+
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars . 0) default-frame-alist)
