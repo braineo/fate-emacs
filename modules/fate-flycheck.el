812571fd551d401660f96de3d830f5ad728e80e4
Binbin Ye <binbin.ye@mujin.co.jp>
2023-04-11 10:05:30 +0900

    add ruff checker and spell checker

--- modules/fate-flycheck.el
+++ modules/fate-flycheck.el
@@ -50,7 +50,31 @@ See URL `https://stedolan.github.io/jq/'."
               (message) "at line " line ", column " column
               (zero-or-more not-newline) line-end))
       :modes fate-json-mode)
-    (add-to-list 'flycheck-checkers 'fate-json-jq))
+    (add-to-list 'flycheck-checkers 'fate-json-jq)
+
+    (flycheck-define-checker python-ruff
+      "A Python syntax and style checker using the ruff utility.
+To override the path to the ruff executable, set
+`flycheck-python-ruff-executable'.
+See URL `http://pypi.python.org/pypi/ruff'."
+        :command ("ruff"
+                  "--format=text"
+                  (eval (when buffer-file-name
+                          (concat "--stdin-filename=" buffer-file-name)))
+                  "-")
+     :standard-input t
+     :error-filter (lambda (errors)
+                     (let ((errors (flycheck-sanitize-errors errors)))
+                       (seq-map #'flycheck-flake8-fix-error-level errors)))
+     :error-patterns
+     ((warning line-start
+               (file-name) ":" line ":" (optional column ":") " "
+               (id (one-or-more (any alpha)) (one-or-more digit)) " "
+               (message (one-or-more not-newline))
+               line-end))
+     :modes python-mode)
+    (add-to-list 'flycheck-checkers 'python-ruff))
+
   :custom
   (flycheck-indication-mode 'right-fringe)
   (flycheck-emacs-lisp-load-path 'inherit)
