5ff19b1007b85330658e5c568da96e2b6ffe7e1a
Binbin Ye <binbin.ye@mujin.co.jp>
2023-04-19 14:46:59 +0900

    fix bootstrap of straight for emacs29

--- core/core-packages.el
+++ core/core-packages.el
@@ -44,19 +44,19 @@
 (setq use-package-always-ensure t)
 
 ;; bootstrap `straight`
+(setq straight-repository-branch "develop")
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
+      (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
-
 (straight-use-package 'use-package)
 
 ;; Only enable for benchmarking
