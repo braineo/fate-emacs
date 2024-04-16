bd09173b5a59d02a1546e1401b85ed7fff361a2f
Binbin Ye <binbin.ye@mujin.co.jp>
2024-04-16 17:29:42 +0900

    add font lock faces
    close #19

---
+++ core/fate-hints.el
@@ -0,0 +1,37 @@
+;;; fate-hints.el --- hints                          -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  binbin
+
+;; Author: Binbin Ye
+;; Keywords: hints
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Some useful hints / key bindings I ues less frequently and need some reminding.
+
+;;; Code:
+
+
+(defvar fate/emacs-hints
+  '("Start and finish recording keyboard macros: C-x (, C-x )"
+    "Enable edit mode in occur mode by `e` (occur-edit-mode)"
+    "Start wgrep in embark export by C-c C-p")
+   "Hints to show on dashboard footer.")
+
+
+
+(provide 'fate-hints)
+;;; fate-hints.el ends here
