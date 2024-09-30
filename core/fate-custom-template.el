06b07b46117e89148fa3c9b845044bacf56cd9ba
Binbin Ye <phantom2501@gmail.com>
2024-09-30 23:52:37 +0900

    prefer LXGW Neo XiHei over Noto

--- core/fate-custom-template.el
+++ core/fate-custom-template.el
@@ -14,10 +14,15 @@
     ((font-installed-p "JetBrains Mono") (set-face-attribute 'default nil :font "JetBrains Mono" :height 75))
     ((font-installed-p "Operator Mono") (set-face-attribute 'default nil :font "Operator Mono" :height 80 :weight 'semilight)))
 
-  (when (font-installed-p "Noto Sans")
-    (set-fontset-font t 'han "Noto Sans CJK SC Regular")
-    (set-fontset-font t 'kana "Noto Sans CJK JP Regular")
-    (set-fontset-font t 'cjk-misc "Noto Sans CJK Sc Regular"))
+  (cond
+   ((font-installed-p "LXGW Neo XiHei")
+    (dolist (char '(han kana cjk-misc))
+      (set-fontset-font t char "LXGW Neo XiHei")))
+   ((font-installed-p "Noto Sans")
+    (progn
+      (set-fontset-font t 'han "Noto Sans CJK SC Regular")
+      (set-fontset-font t 'kana "Noto Sans CJK JP Regular")
+      (set-fontset-font t 'cjk-misc "Noto Sans CJK Sc Regular"))))
 
   (when (font-installed-p "Noto Color Emoji")
     (set-fontset-font t 'emoji (font-spec :family "Noto Color Emoji") nil 'prepend)))
