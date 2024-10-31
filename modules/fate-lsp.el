f3e42cf7e08bfbcf42c25fe8cebafc19933e1477
Binbin Ye <binbin.ye@mujin.co.jp>
2024-10-31 10:16:57 +0900

    use built-in :vc in use-package

--- modules/fate-lsp.el
+++ modules/fate-lsp.el
@@ -30,12 +30,7 @@
 
 
 (use-package lsp-bridge
-  :straight '(lsp-bridge
-               :type git
-               :host github
-               :repo "manateelazycat/lsp-bridge"
-               :files ("*" (:exclude ".git"))
-               :build (:not compile))
+  :vc (:url "https://github.com/manateelazycat/lsp-bridge")
   :hook (after-init . global-lsp-bridge-mode)
   :config
   (add-to-list
