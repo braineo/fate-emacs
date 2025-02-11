ac5f006ac40d2289cf9f3f210fd2508491922167
Binbin Ye <binbin.ye@mujin.co.jp>
2025-02-11 12:28:02 +0900

    improve debug print

--- bin/update_forge_repos.el
+++ bin/update_forge_repos.el
@@ -16,6 +16,9 @@
 (defvar forge-pull-total 0
   "Number of completed `forge-pull` operations.")
 
+(defvar forge-pull-repos '()
+  "Remaining repos")
+
 (pcase-dolist (`(,name ,id)
                (forge-sql [:select [name id] :from repository
                            ;;:where (= class 'gitlab)
@@ -23,8 +26,15 @@
   (progn
     (setq forge-pull-total (1+ forge-pull-total))
     (when-let ((repo (forge-get-repository :id id))
+               (name (oref repo name))
                ((forge-get-repository repo :tracked?)))
-      (forge--pull repo (lambda () (setq forge-pull-completed (1+ forge-pull-completed)))))))
+      (progn
+       (add-to-list 'forge-pull-repos name
+        (forge--pull repo (lambda ()
+                            (setq forge-pull-completed (1+ forge-pull-completed))
+                            (delete name forge-pull-repos)
+                            (when (length> forge-pull-repos 0)
+                              (print (format "renaming repos %s" forge-pull-repos))))))))))
 
 (while (< forge-pull-completed forge-pull-total)
   (sleep-for 1))
