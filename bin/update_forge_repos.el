9acce661b9e21b95a50dee972b89d9a4a6dcc2da
Binbin Ye <binbin.ye@mujin.co.jp>
2026-09-17 12:02:07 +0800

    show progress when pulling repos

--- bin/update_forge_repos.el
+++ bin/update_forge_repos.el
@@ -8,6 +8,9 @@
 
 (require 'forge)
 
+;; messages are re-emitted by `forge-pull--log', so the bar stays last
+(setq inhibit-message t)
+
 (setq forge-database-file "~/.emacs.d/var/forge/database.sqlite")
 
 (defvar forge-pull-completed 0
@@ -19,10 +22,34 @@
 (defvar forge-pull-repos '()
   "Remaining repos")
 
+(defconst forge-pull-tty (not (member (getenv "TERM") '(nil "dumb")))
+  "Non-nil on a real terminal; from cron just print the log lines.")
+
+(defun forge-pull--report ()
+  "Redraw a one-line progress bar in place on stdout."
+  (when forge-pull-tty
+    (let* ((width 30)
+           (done (/ (* width forge-pull-completed) (max 1 forge-pull-total))))
+      (send-string-to-terminal
+       (format "\r[%s%s] %d/%d %s\e[K"
+               (make-string done ?=)
+               (make-string (- width done) ?\s)
+               forge-pull-completed forge-pull-total
+               (string-join (last forge-pull-repos 3) " "))))))
+
+(defun forge-pull--log (fmt &rest args)
+  "Print a log line above the progress bar, then redraw the bar."
+  (when fmt
+    (send-string-to-terminal
+     (concat (if forge-pull-tty "\r\e[K" "") (apply #'format-message fmt args) "\n"))
+    (forge-pull--report)))
+
+(advice-add 'message :after #'forge-pull--log)
+
 (pcase-dolist (`(,name ,id)
                (forge-sql [:select [name id] :from repository
-                           :where (= condition ':tracked)
-                           :order-by [(asc owner) (asc name)]]))
+				   :where (= condition ':tracked)
+				   :order-by [(asc owner) (asc name)]]))
   (progn
     (when-let ((repo (forge-get-repository :id id))
                (name (oref repo name)))
@@ -31,8 +58,9 @@
       (forge--pull repo (lambda (_)
                           (setq forge-pull-completed (1+ forge-pull-completed))
                           (setq forge-pull-repos (delete name forge-pull-repos))
-                          (when (length> forge-pull-repos 0)
-                            (print (format "Remaining repos %s" forge-pull-repos))))))))
+                          (forge-pull--report))))))
 
+(forge-pull--report)
 (while (< forge-pull-completed forge-pull-total)
   (sleep-for 1))
+(send-string-to-terminal "\n")
